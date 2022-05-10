1f9dd983d652843a700eb3cf695a2cfbc5718594
chương lương <luongvanchuong963@gmail.com>
2022-05-10 08:08:30 +0700

    btc#

---
+++ Baitapc-luong-chuong/Bai2.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BTDIEMDANH
+{
+    class Bai2
+    {
+        static void Main(string[] args)
+        {
+            Cat cat205 = new Cat();
+            cat205.eat();
+            cat205.makeSound();
+            cat205.run();
+            Bird bird205 = new Bird();
+            bird205.eat();
+            bird205.makeSound();
+            bird205.fly();
+            Console.ReadKey();
+        }
+    }
+    class Animal
+    {
+        public virtual void eat()
+        {
+            Console.WriteLine("Foot");
+        }
+        public virtual void makeSound()
+        {
+            Console.WriteLine("");
+        }
+    }
+    class Cat : Animal
+    {
+        public void run()
+        {
+            Console.WriteLine("Meo chay nhanh");
+
+        }
+        public override void eat()
+        {
+            Console.WriteLine("Meo an ca");
+        }
+        public override void makeSound()
+        {
+            Console.WriteLine("Meo keu meo meo");
+        }
+    }
+    class Bird : Animal
+    {
+        public void fly()
+        {
+            Console.WriteLine("Chim bay cao");
+
+        }
+        public override void eat()
+        {
+            Console.WriteLine("Chim an sau");
+        }
+        public override void makeSound()
+        {
+            Console.WriteLine("Chim an con trung");
+        }
+    }
+}
---
+++ Baitapc-luong-chuong/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace BTDIEMDANH
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhap a,b,c: ");
+            double a205 = Int32.Parse(Console.ReadLine());
+            double b205 = Int32.Parse(Console.ReadLine());
+            double c205 = Int32.Parse(Console.ReadLine());
+            ptb2 pt = new ptb2(a205, b205, c205);
+
+            double delta = pt.delta(a205, b205, c205);
+            int check205 = pt.nghiem(delta);
+            if (check205 < 0)
+            {
+                Console.WriteLine("Pt vo nghiem");
+            }
+            else if (check205 == 0)
+            {
+                double x = -b205 / (2 * a205);
+                Console.WriteLine("pt co nghiem kep: ", x);
+            }
+            else
+            {
+                double x1 = (-b205 - Math.Sqrt(delta)) / (2 * a205);
+                double x2 = (-b205 + Math.Sqrt(delta)) / (2 * a205);
+
+                Console.WriteLine("Pt co 2 nghiem x1={0} , x2={1}: ", x1, x2);
+            }
+        }
+    }
+
+    class ptb2
+    {
+        private double a205;
+        private double b205;
+        private double c205;
+
+        public ptb2()
+        {
+        }
+
+        public ptb2(double a205, double b205, double c205)
+        {
+            A = a205;
+            B = b205;
+            C = c205;
+        }
+
+        public double A { get; set; }
+        public double B { get; set; }
+        public double C { get; set; }
+
+        public double delta(double a205, double b205, double c205)
+        {
+            return b205 * b205 - 4 * a205 * c205;
+        }
+
+        public int nghiem(double delta205)
+        {
+            if (delta205 < 0)
+            {
+                return -1;
+            }
+            else if (delta205 == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+    }
+}
